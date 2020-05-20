01039d9753773056e0bc028a9a8fd623d147e6ee
Roman-tic <61428860+Roman-tic@users.noreply.github.com>
2020-05-20 19:29:15 +0600

    Add files via upload

--- test.php
+++ test.php
@@ -15,3 +15,16 @@ $arr = mysqli_query($con, "INSERT INTO `articles_categories` (`id`,`title`) VALU
 $rra = mysqli_query($con, 'SELECT * FROM `articles_categories`');
 
 ?>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+<html>
+ <head>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+  <title>Пример веб-страницы</title>
+ </head>
+ <body>
+  <h1>Заголовок</h1>
+<?php echo 'хмм работает';
+?>
+  
+ </body>
+</html>
